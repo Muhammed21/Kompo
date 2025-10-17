1af99c2c3535bc8f8cea2037f0ab481ae28c7069
Muhammed Cavus <mcavus048@gmail.com>
2025-10-17 16:18:22 +0200

    feat(package): Add get-all-slug.interface for getAllSlugs function

--- packages/kompo-types/src/index.ts
+++ packages/kompo-types/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./docs/dto/get-docs-with-id.dto";
-export * from "./docs/interface/get-docs-with-id.interface";
 export * from "./accessibility/dto/accessibility-scan.dto";
+export * from "./docs/interface/get-docs-with-id.interface";
+export * from "./docs/interface/get-all-slug.interface";
